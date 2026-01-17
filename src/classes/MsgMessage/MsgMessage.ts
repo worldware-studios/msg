057a319b9a0a08947c976ac1544bfac847d2c92c
Joel Sahleen <joel@sahleen.net>
2026-01-17 10:05:16 -0700

    Refactor translation logic and enhance test coverage

--- src/classes/MsgMessage/MsgMessage.ts
+++ src/classes/MsgMessage/MsgMessage.ts
@@ -45,56 +45,28 @@ export class MsgMessage implements MsgInterface {
     return this._key;
   }
 
-  public set key(key: string) {
-    this._key = key;
-  }
-
   public get value() {
     return this._value;
-  }
-
-  public set value(value: string) {
-    this._value = value;
   }
 
   public get attributes() {
     return this._attributes;
   }
 
-  public set attributes(attributes: MsgAttributes) {
-    this._attributes = attributes;
-  }
-
   public get lang() {
    return this._attributes['lang'];
-  }
-
-  public set lang(lang: string | undefined) {
-    this._attributes.lang = lang;
   }
 
   public get dir() {
    return this._attributes['dir'];
   }
 
-  public set dir(dir: string | undefined) {
-    this._attributes.dir = dir;
-  }
-
   public get dnt() {
    return this._attributes['dnt'];
   }
 
-  public set dnt(dnt: boolean | undefined) {
-    this._attributes.dnt = dnt;
-  }
-
   public get notes() {
     return this._notes;
-  }
-
-  public set notes(notes: MsgNote[]) {
-    this._notes = notes;
   }
 
   public addNote(note: MsgNote) {
