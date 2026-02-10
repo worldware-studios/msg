73b1ba353e35150fc5e7fb6cf68507099cb205a6
Joel Sahleen <jsahleen@users.noreply.github.com>
2026-02-10 14:51:22 -0700

    Update build configuration and dependencies (#23)
    - Added 'tsup' as a build tool in package.json and updated the build script to use it.
    - Updated TypeScript target version from 'es2020' to 'esnext' in tsconfig.json for improved compatibility.
    - Introduced a new tsup.config.ts file to define build settings, including output formats and type generation.
    - Updated package-lock.json to reflect the addition of new dependencies and their versions.

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: [
+    'src/**/*',
+    '!src/tests/**/*',
+  ],
+  format: ['cjs', 'esm'], // Output both
+  dts: true,              // Generate types
+  clean: true,            // Clean dist
+  target: 'esnext',
+  outExtension({ format }) {
+    return {
+      js: format === 'esm' ? '.mjs' : '.cjs',
+    }
+  }
+})
